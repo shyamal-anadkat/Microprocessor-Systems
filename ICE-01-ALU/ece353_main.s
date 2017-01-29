247334cb5783d75f2fbfe3803694667b625ac425
zostale <shyamalanadkat@gmail.com>
2017-01-29 16:39:25 -0600

    SA 1/29 first commit

---
+++ ICE-01-ALU/ece353_main.s
@@ -0,0 +1,99 @@
+; Filename:     main.s 
+; Author:       Shyamal Anadkat
+; Description:  ICE01
+
+    export __main
+
+;**********************************************
+; SRAM
+;**********************************************
+    AREA    SRAM, READWRITE
+    align
+        
+;**********************************************
+; Constant Variables (FLASH) Segment
+;**********************************************
+    AREA    FLASH, CODE, READONLY
+    align
+
+;**********************************************
+; Code (FLASH) Segment
+; main assembly program
+;**********************************************
+__main   PROC
+    
+    ; DO NOT MODIFY ANTHING ABOVE THIS LINE!!! 
+   
+    
+    ; CHECK POINT (1)
+    ; Initialize R1 to be 20 (decimal)
+	MOV  R1, #20
+    
+    ; CHECK POINT (2)
+    ; Add 15 to R1 using an immediate
+	ADD R1, R1, #15
+    
+    ; CHECK POINT (3)
+    ; Multiply R1 by 3
+	MOV R0, #3
+	MUL R1, R1, R0
+
+    ; CHECK POINT (4)
+    ; Clear (set to 0) bits 4:0 of R1
+    ; All other bits should not be affected
+	BIC R1, R1, #0x1F
+
+    ; CHECK POINT (5)
+    ; Set bits 31:25 and 21:16 of R1 to be a 1
+    ; All other bits should not be affected
+	MOVT R3, #0xFE34
+	ORR R1, R1, R3
+    
+    ; CHECK POINT (6)
+    ; Invert ONLY bits 3-9 of R1
+	MOV R7, #0x03F8
+	EOR R1, R7, R1
+	
+
+    ; CHECK POINT (7) 
+    ; Move the contents of R1 to R0
+	MOV R0, R1
+    
+    ; CHECK POINT (8) 
+    ; Total the bytes in R0 and place the results in R2
+    ; R2 = R0[31:24] + R0[23:16] + R0[15:8] + R0[7:0]
+    ; Treat each byte an an unsigned 8-bit number
+	AND R8, R0, #0x000000FF 
+	AND R9, R0, #0x0000FF00
+	ASR R9, R9, #8
+	ADD R10, R8, R9
+	AND R11, R0, #0x00FF0000
+	ASR R11, R11, #16
+	AND R12, R0, #0xFF000000
+	ASR R12, R12, #24 
+	ADD R4, R11, R12
+	ADD R2, R4, R10
+
+    ; CHECK POINT (9) 
+    ; Initialize R4 to be decimal value 100
+    ; Initialize R5 to be decimal value 110
+    ; Without using the subtract instruction, subtract R5 from R4
+    ; and place the results in R6.  Use as many ARM assembly 
+    ; instructions as needed.
+	MOV R4, #100
+	MOV R5,#110  
+	MVN	R5, R5
+	ADD R5, R5, #1
+	ADD R6, R5, R4
+    
+    ; DO NOT MODIFY ANTHING BELOW THIS LINE!!!
+        
+INFINITE_LOOP
+    B   INFINITE_LOOP
+    ENDP
+    align
+        
+
+    END
+        
+        
---
+++ lib/system_tm4c123.d
@@ -0,0 +1,9 @@
+..\lib\system_tm4c123.o: RTE\Device\TM4C123GH6PM\system_TM4C123.c
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C123\TM4C123.h
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C123\TM4C123GH6PM.h
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\Pack\ARM\CMSIS\3.20.4\CMSIS\Include\core_cm4.h
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\Pack\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmInstr.h
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\Pack\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmFunc.h
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\Pack\ARM\CMSIS\3.20.4\CMSIS\Include\core_cm4_simd.h
+..\lib\system_tm4c123.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C123\system_TM4C123.h
